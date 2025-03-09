5bf22e8a0350fbe55c9c500a5e655d6de9a03f4f
ekim1832 <ekim_sm@hotmail.com>
2025-03-09 17:49:54 -0500

    Commit inicial

--- config/database.js
+++ config/database.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/Jefazo-comanda', {
-     // useNewUrlParser: true,
+    const conn = await mongoose.connect(process.env.MONGODB_URI)  
+/*   const conn = await mongoose.connect('mongodb://localhost:27017/Jefazo-comanda', {
+  
+ // useNewUrlParser: true,
     // useUnifiedTopology: true,
       // Configuración para mejor rendimiento
      // poolSize: 50, // Aumenta el pool de conexiones
       autoIndex: true, // Mantiene índices para búsquedas rápidas
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
-    });
+    });*/
     console.log(`MongoDB Conectado: ${conn.connection.host}`);
   } catch (err) {
     console.error('Error de conexión MongoDB:', err.message);
